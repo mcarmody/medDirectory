d3254ac1f5bf74a8a0094b3a9ccfa7b7f6962421
mcarmody <Mike@Michaels-MacBook-Pro.local>
2015-02-23 14:17:05 -0600

    initial commit

---
+++ www/js/controllers.js
@@ -0,0 +1,28 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope) {})
+
+.controller('ChatsCtrl', function($scope, Chats) {
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  }
+})
+
+.controller('DevicesCtrl', function($scope, Devices) {
+  $scope.devices = Devices.all;
+})
+
+.controller('DeviceInfoCtrl', function($scope, $stateParams, Devices) {
+  $scope.devices = Devices.get($stateParams.deviceId);
+})
+
+.controller('CategoriesCtrl', function($scope,Categories) {
+  $scope.categories = Categories.all;
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
